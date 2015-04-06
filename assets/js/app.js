601bb1e755f97cf928a9f3cb2ac3c45e60713b07
tsimawei <turner.simawei@gmail.com>
2015-04-06 14:43:44 -0400

    Add Geocoder and Search Bar to change Map Center

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,110 @@
-function initMap(location, element) {
+
+// Creates global-esque variables for function access
+var map;
+var geocoder = new google.maps.Geocoder();
+
+function initMap(element) {
 
 	// Sets default element if no element is passed into function
 	if (!element) {
 		element = document.getElementById('map-canvas');
 	}
-	
-	// Sets default location if no location is passed into function
-	if (!location) {
-		location = {lat: '37.7386972', lng: '-122.4104775', zoom: '12'};
-	}
 
-	// Parses location object to mapOptions for map parameters
-	var mapOptions = {
-	  center: { lat: parseFloat(location.lat), lng: parseFloat(location.lng)},
-	  zoom: parseInt(location.zoom)
-	};
+	// Creates map object
+	var map = new google.maps.Map(element);
+
+	// Sets default location
+	var location = new google.maps.LatLng(-34.397, 150.644);
 	
-	// returns new map object with defined location and zoom parameters
-	return new google.maps.Map(element, mapOptions);
+	// Calls updateMap function to set map to default location
+	updateMap(map, location);
+	
+	// returns map object
+	return map
 }
 
+function updateMap(map, location) {
+	map.setCenter(location);
+	map.setZoom(10);
+}
 
+function codeAddress(map, address) {
 
-var viewModel = function () {
+	// Geocodes address to provide support for string addresses
+	geocoder.geocode({ 'address': address }, function(results, status) {
+  		if (status == google.maps.GeocoderStatus.OK) {
+	    	updateMap(map, results[0].geometry.location);
+	  	} else {
+	  		errorReturn('geocoder');
+	  	}
+  	});
+}
+
+function errorReturn(error) {
+	if (error === 'geocoder') {
+		console.log(error);
+	} else {
+		console.log(error);
+	}
+}
+
+var ViewModel = function () {
 	var self = this;
 
-	// sets current map for marker reference
-	self.currentMap = initMap(location, mapElement);
+	self.init = function() {
+		self.currentMap = initMap();
+	};
+
+	self.googleSearch = ko.observable();
+
+	self.googleCode = function() {
+		codeAddress(self.currentMap, self.googleSearch());
+	};
+
+	self.checkEnter = function(data, e) {
+		if (e.keyCode === 13) {
+			self.googleCode();
+		}
+		return true;
+	};
+
+	// wikipedia results
+	self.wikiResult = function() {
+		var wikiRequestTimeout = setTimeout(function(){
+        	$wikiElem.text("failed to get wikipedia resources");
+    	}, 8000);
+
+		var wikiAPIurl = "http://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=" + address;
+	    $.ajax( {
+	        url: wikiAPIurl,
+	        dataType: 'jsonp',
+	        success: function (data, textStatus, jqXHR) {
+	            var items = [];
+	            console.log(data);
+	            $.each(data.query.search, function ( index, article) {
+	                items.push('<li class=article><a href="http://en.wikipedia.org/wiki/'+ article.title + '">' +
+	                    article.title + '</a></li>');
+	            });
+	            clearTimeout(wikiRequestTimeout);
+	            $wikiElem.append(items.join( "" ));
+
+	        },
+	    } );
+	};
 
 	// Initialize marker array for Google Map Marker objects
-	self.markers = ko.observableArray([]);
+	//self.markers = ko.observableArray([]);
 
 	// Add markers to map from data array
-	markerList.forEach(function(markerItem) {
+	/*markerList.forEach(function(markerItem) {
 		var marker = new google.maps.Marker({
 			position : markerItem.position,
 			map : currentMap,
 			title : markerItem.title
 		});
-	});
+	});*/
+
+	google.maps.event.addDomListener(window, 'load', self.init());
 };
 
-google.maps.event.addDomListener(window, 'load', initMap());
+ko.applyBindings(new ViewModel());
